ab39aabab6fac65e88615c7e21909ce458388683
MVZ <drmvz@mail.ru>
2023-09-10 15:38:45 +0300

    fin

---
+++ test.js
@@ -0,0 +1,19 @@
+// function ShowName(names){
+//     alert(`Привет ${names}`);
+// }
+// let names = prompt("Как Вас зовут?");
+// if(names == ' ')
+//     ShowName();
+// else
+//     ShowName(names);
+
+confirm
+if (confirm("Вы уверены?"))
+    alert("Мы рады, что вы уверены!")
+else
+    alert("Жаль, что вы неуверены")
+
+
+
+
+
